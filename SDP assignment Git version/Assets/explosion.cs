e8b0f60921eff4e6acdd064857feaf96d888e415
19Maaka <62680289+19Maaka@users.noreply.github.com>
2020-05-16 18:18:43 +0800

    opening menu, effect
    opening menu added, now bullet will explose and animation and will remove over time

---
+++ SDP assignment Git version/Assets/explosion.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class explosion : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Explosion());
+    }
+
+    IEnumerator Explosion()
+    {
+        yield return new WaitForSeconds(0.4f);
+        Destroy(gameObject);
+    }
+}

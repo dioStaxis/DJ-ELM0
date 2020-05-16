97a1ab081d979d9a462b5193a181edbb76227986
19Maaka <62680289+19Maaka@users.noreply.github.com>
2020-05-16 13:31:18 +0800

    animation, shooting added

--- SDP assignment Git version/Assets/playerMovement.cs
+++ SDP assignment Git version/Assets/playerMovement.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class playerMovement : MonoBehaviour
 {
     public player_controller controller;
+    public Animator animator;
+
     float horizontalmove = 0f;
     public float runspeed = 40f;
     bool jump = false;
@@ -14,9 +16,12 @@ public class playerMovement : MonoBehaviour
     {
         horizontalmove = Input.GetAxisRaw("Horizontal")*runspeed;
 
+        animator.SetFloat("speed", Math.Abs(horizontalmove));
+
         if (Input.GetButtonDown("Jump"))
         {
             jump = true;
+            animator.SetBool("Jumping", true);
         }
         if (Input.GetButtonDown("crouch"))
         {
@@ -26,6 +31,17 @@ public class playerMovement : MonoBehaviour
             crouch = false;
         }
     }
+
+    public void OnLanding()
+    {
+        animator.SetBool("Jumping", false);
+    }
+
+    public void OnWalking(bool isWalking)
+    {
+        animator.SetBool("Walking", isWalking);
+    }
+
 
     private void FixedUpdate()
     {

388c7d1f6954ca217949c5de06fcfad942382c9f
19Maaka <62680289+19Maaka@users.noreply.github.com>
2020-05-22 11:02:24 +0800

    health bar HUD

--- SDP assignment Git versionV2/Assets/script/healthBar.cs
+++ SDP assignment Git versionV2/Assets/script/healthBar.cs
@@ -1,20 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class healthBar : MonoBehaviour
 {
     public Slider slider;
+    public Gradient gradient;
+    public Image fill;
     
     public void setHealth(int health)
     {
         slider.value = health;
+
+        fill.color = gradient.Evaluate(slider.normalizedValue);
     }
 
     public void setMaxHealth(int health)
     {
         slider.maxValue = health;
         slider.value = health;
+
+        fill.color = gradient.Evaluate(1f);
     }
 }

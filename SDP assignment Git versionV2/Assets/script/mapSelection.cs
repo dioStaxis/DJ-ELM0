1c4615f9991a665f4624105042b1f23c0375839e
Frozensapphire <61902105+Frozensapphire@users.noreply.github.com>
2020-05-23 23:19:54 +1200

    maps
    Made a new map and map selection scene

---
+++ SDP assignment Git versionV2/Assets/script/mapSelection.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mapSelection : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Map1()
+    {
+        SceneManager.LoadScene("ingame");
+    }
+    public void Map2()
+    {
+        SceneManager.LoadScene("map2");
+    }
+
+    public void Back()
+    {
+        SceneManager.LoadScene("Main menu");
+    }
+
+}

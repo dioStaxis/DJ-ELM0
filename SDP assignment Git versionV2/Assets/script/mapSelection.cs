42e1f41aa6db0db490fc8be81462c8f3aa504da8
19Maaka <62680289+19Maaka@users.noreply.github.com>
2020-06-23 10:28:57 +0800

    updated

--- SDP assignment Git versionV2/Assets/script/mapSelection.cs
+++ SDP assignment Git versionV2/Assets/script/mapSelection.cs
@@ -8,11 +8,15 @@ public class mapSelection : MonoBehaviour
     // Start is called before the first frame update
     public void Map1()
     {
-        SceneManager.LoadScene("ingame");
+        SceneManager.LoadScene("map1");
     }
     public void Map2()
     {
         SceneManager.LoadScene("map2");
+    }
+    public void AIMap()
+    {
+        SceneManager.LoadScene("Ai scene");
     }
 
     public void Back()

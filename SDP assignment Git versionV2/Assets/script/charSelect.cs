a197ccffcda169c38f171707d041b19e960814fb
Frozensapphire <61902105+Frozensapphire@users.noreply.github.com>
2020-06-23 17:51:24 +1200

    sprint 2

---
+++ SDP assignment Git versionV2/Assets/script/charSelect.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class charSelect : MonoBehaviour
+{
+    public void char1Map1()
+    {
+        SceneManager.LoadScene("map1");
+    }
+    public void char1Map2()
+    {
+        SceneManager.LoadScene("map2");
+    }
+    public void char2Map1()
+    {
+        SceneManager.LoadScene("map1Owl");
+    }
+
+    public void char2Map2()
+    {
+        SceneManager.LoadScene("map2Owl");
+    }
+
+    public void Back()
+    {
+        SceneManager.LoadScene("mapSelect");
+    }
+}
